c4d7bf00c6798969ed50e980765a6ec37841bbb1
ipcrm <mcadorette@puppetlabs.com>
2016-08-29 14:47:18 -0400

    single report full log

--- lib/puppet/reports/elk_report.rb
+++ lib/puppet/reports/elk_report.rb
@@ -7,6 +7,7 @@ Puppet::Reports.register_report(:elk_report) do
   def process
     client = Elasticsearch::Client.new host: 'localhost'
     index_date = Time.now.utc.to_date
+    messages = Array.new
 
     self.resource_statuses.each do |k,v|
       puts v.resource_type
@@ -19,28 +20,26 @@ Puppet::Reports.register_report(:elk_report) do
     #Notice: /Stage[main]/Main/File[/tmp/foo]/ensure: created
     list=self.logs
     list.each_with_index do |l,i|
-      message = "#{l.level}: "
+      message = "#{l.level.capitalize}: "
       message << "#{l.source}: " if l.source and l.source != 'Puppet'
       message << l.message
-
-      client.create index: "puppet-#{index_date}",
-        type: 'log',
-        body: {
-         puppet_configuration_version: "config version #{self.configuration_version}",
-         puppet_environment: self.environment,
-         puppet_message: message,
-         puppet_file: l.file,
-         puppet_line: l.line,
-         puppet_version: "#{self.puppet_version}",
-         puppet_status: self.status,
-         puppet_host: self.host,
-         puppet_time: self.time,
-         puppet_type: self.kind,
-         puppet_transaction: self.transaction_uuid,
-         tags: ['puppet', 'puppet_log'],
-         published: true,
-         published_at: Time.now.utc.iso8601,
-        }
+      messages << message
     end
+
+    client.create index: "puppet-#{index_date}",
+      type: 'log',
+      body: {
+       puppet_configuration_version: "config version #{self.configuration_version}",
+       puppet_environment: self.environment,
+       puppet_message: messages,
+       puppet_version: "#{self.puppet_version}",
+       puppet_status: self.status,
+       puppet_host: self.host,
+       puppet_time: self.time,
+       puppet_type: self.kind,
+       tags: ['puppet', 'puppet_log'],
+       published: true,
+       published_at: Time.now.utc.iso8601,
+      }
   end
 end

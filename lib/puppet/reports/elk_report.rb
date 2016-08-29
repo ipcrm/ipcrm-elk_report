544df9c7612f6611b2dfeb662dd3187d8214ec8e
ipcrm <mcadorette@puppetlabs.com>
2016-08-29 16:33:39 -0400

    Baseline working events/reports

--- lib/puppet/reports/elk_report.rb
+++ lib/puppet/reports/elk_report.rb
@@ -1,32 +1,58 @@
 require 'puppet'
 require 'elasticsearch'
+require 'date'
 
 Puppet::Reports.register_report(:elk_report) do
   desc "Send reports to ELK stack"
 
   def process
-    client = Elasticsearch::Client.new host: 'localhost'
-    index_date = Time.now.utc.to_date
-    messages = Array.new
 
+    # Create Connnection Object with Elasticsearch
+    client = Elasticsearch::Client.new host: 'localhost'
+
+    # Create a new document for each resource that was managed
     self.resource_statuses.each do |k,v|
-      puts v.resource_type
-      puts v.title
-      v.events.each do |e|
-        puts e.name
+      resource_body = Hash.new
+      resource_body["puppet_host"]      = self.host
+      resource_body["puppet_path"]      = "#{v.resource_type.capitalize}[#{v.title}]"
+      resource_body["puppet_time"]      = v.time
+      resource_body["puppet_tags"]      = v.tags
+      resource_body["puppet_eval_time"] = v.evaluation_time
+      resource_body["puppet_file"]      = v.file
+      resource_body["puppet_line"]      = v.line
+      resource_body["published"]        = true
+      resource_body["published_at"]     = Time.now.utc.iso8601
+
+      if self.report_format >= 5
+        resource_body["puppet_uuid"] = self.transaction_uuid
+      else
+        resource_body["puppet_uuid"] = nil
       end
+
+      if v.events[0]
+        resource_body["puppet_message"] = v.events[0].message
+        resource_body["puppet_status"]  = v.events[0].status
+      else
+        resource_body["puppet_message"] = nil
+        resource_body["puppet_status"]  = :unchanged
+      end
+
+      client.create index: "puppet_resource-#{Time.now.utc.to_date}",
+        type: 'event',
+        body: resource_body
     end
 
-    #Notice: /Stage[main]/Main/File[/tmp/foo]/ensure: created
-    list=self.logs
-    list.each_with_index do |l,i|
+
+    # Create new document for the log view of the run
+    messages = Array.new
+    self.logs.each_with_index do |l,i|
       message = "#{l.level.capitalize}: "
       message << "#{l.source}: " if l.source and l.source != 'Puppet'
       message << l.message
       messages << message
     end
 
-    client.create index: "puppet-#{index_date}",
+    client.create index: "puppet-#{Time.now.utc.to_date}",
       type: 'log',
       body: {
        puppet_configuration_version: "config version #{self.configuration_version}",

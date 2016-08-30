1f03feb61d7b9b6aff5e11be006141cbc411fc4d
ipcrm <mcadorette@puppetlabs.com>
2016-08-29 22:13:15 -0400

    Expose user/group as params

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,6 @@
 class elk_report (
+  String $user           = $::settings::user,
+  String $group          = $::settings::group,
   Hash $elkreport_config = undef,
 ){
   # Configure Puppet to use elk_report setting
@@ -13,8 +15,8 @@ class elk_report (
   if $elkreport_config != undef {
     file {"${::settings::confdir}/elk_report.yaml":
       ensure  => present,
-      owner   => $::settings::user,
-      group   => $::settings::group,
+      owner   => $user,
+      group   => $group,
       content => template('elk_report/elk_report.yaml.erb'),
     }
   }

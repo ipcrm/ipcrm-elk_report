c3fc92b84bb47eb420134995b6d3937ee8533023
ipcrm <mcadorette@puppetlabs.com>
2016-08-29 22:37:04 -0400

    updated perms

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,6 @@
 class elk_report (
-  String $user           = $::settings::user,
-  String $group          = $::settings::group,
+  String $user           = $::id,
+  String $group          = $::gid,
   Hash $elkreport_config = undef,
 ){
   # Configure Puppet to use elk_report setting
@@ -15,8 +15,8 @@ class elk_report (
   if $elkreport_config != undef {
     file {"${::settings::confdir}/elk_report.yaml":
       ensure  => present,
-      owner   => $user,
-      group   => $group,
+      owner   => $::id,
+      group   => $::gid,
       content => template('elk_report/elk_report.yaml.erb'),
     }
   }

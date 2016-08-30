afd9d65fa31db008328048eb48a90a9ab2414159
ipcrm <mcadorette@puppetlabs.com>
2016-08-29 22:50:26 -0400

    Adding elasticsearch gem

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,6 +3,19 @@ class elk_report (
   String $group          = $::gid,
   Hash $elkreport_config = undef,
 ){
+  #Install elasticsearch gem
+  package{'elasticsearch':
+    ensure   => present,
+    provider => 'puppet_gem',
+  }
+
+  package{'elasticsearch-puppetserver':
+    ensure   => present,
+    name     => 'elasticsearch',
+    provider => 'puppetserver_gem',
+    notify   => Service['pe-puppetserver'],
+  }
+
   # Configure Puppet to use elk_report setting
   ini_setting { 'puppet agent reports':
     ensure  => present,

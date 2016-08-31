90e772cccef0a4062e22f133629cde47d7796786
ipcrm <mcadorette@puppetlabs.com>
2016-08-31 11:26:33 -0400

    Updating field names
    Remvoing puppet_ preface; these are dedicated indexes there is no
    need to preface

--- lib/puppet/reports/elk_report.rb
+++ lib/puppet/reports/elk_report.rb
@@ -32,28 +32,28 @@ Puppet::Reports.register_report(:elk_report) do
       Puppet.notice("ELK Report: #{self.host} - create a new ElasticSearch document for each Puppet Resource")
       self.resource_statuses.each do |k,v|
         resource_body = Hash.new
-        resource_body["puppet_host"]      = self.host
-        resource_body["puppet_resource"]      = "#{v.resource_type.capitalize}[#{v.title}]"
-        resource_body["puppet_time"]      = v.time
-        resource_body["puppet_tags"]      = v.tags
-        resource_body["puppet_eval_time"] = v.evaluation_time
-        resource_body["puppet_file"]      = v.file
-        resource_body["puppet_line"]      = v.line
-        resource_body["published"]        = true
-        resource_body["published_at"]     = Time.now.utc.iso8601
+        resource_body["host"]         = self.host
+        resource_body["resource"]     = "#{v.resource_type.capitalize}[#{v.title}]"
+        resource_body["time"]         = v.time
+        resource_body["tags"]         = v.tags
+        resource_body["eval_time"]    = v.evaluation_time
+        resource_body["file"]         = v.file
+        resource_body["line"]         = v.line
+        resource_body["published"]    = true
+        resource_body["published_at"] = Time.now.utc.iso8601
 
         if self.report_format >= 5
-          resource_body["puppet_uuid"] = self.transaction_uuid
+          resource_body["uuid"] = self.transaction_uuid
         else
-          resource_body["puppet_uuid"] = nil
+          resource_body["uuid"] = nil
         end
 
         if v.events[0]
-          resource_body["puppet_message"] = v.events[0].message
-          resource_body["puppet_status"]  = v.events[0].status
+          resource_body["log"] = v.events[0].message
+          resource_body["status"]  = v.events[0].status
         else
-          resource_body["puppet_message"] = nil
-          resource_body["puppet_status"]  = :unchanged
+          resource_body["log"] = nil
+          resource_body["status"]  = :unchanged
         end
 
         client.create index: "puppet_resource-#{Time.now.utc.to_date}",
@@ -63,27 +63,27 @@ Puppet::Reports.register_report(:elk_report) do
 
       # Create new document for the log view of the run
       Puppet.notice("ELK Report: #{self.host} - create a new Elasticsearch document for this Puppet transaction")
-      messages = Hash.new
+      logs = Hash.new
       count = 0
       self.logs.each_with_index do |l,i|
-        message = "#{l.level.capitalize}: "
-        message << "#{l.source}: " if l.source and l.source != 'Puppet'
-        message << l.message
-        messages["%03d" % count] = message
+        log = "#{l.level.capitalize}: "
+        log << "#{l.source}: " if l.source and l.source != 'Puppet'
+        log << l.message
+        logs["%03d" % count] = log
         count+=1
       end
 
       client.create index: "puppet-#{Time.now.utc.to_date}",
         type: 'log',
         body: {
-         puppet_configuration_version: "config version #{self.configuration_version}",
-         puppet_environment: self.environment,
-         puppet_messages: messages,
-         puppet_version: "#{self.puppet_version}",
-         puppet_status: self.status,
-         puppet_host: self.host,
-         puppet_time: self.time,
-         puppet_type: self.kind,
+         configuration_version: "config version #{self.configuration_version}",
+         environment: self.environment,
+         log_entry: logs,
+         agent_version: "#{self.puppet_version}",
+         status: self.status,
+         host: self.host,
+         time: self.time,
+         type: self.kind,
          tags: ['puppet', 'puppet_log'],
          published: true,
          published_at: Time.now.utc.iso8601,
